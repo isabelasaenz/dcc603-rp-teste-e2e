80d83580dc2263dd57a8132b2ee0b59904369e05
Isabela Saenz <isabelasaenzcardoso@gmail.com>
2025-06-22 13:52:23 +0000

    adiciona testes E2E para a aplicação

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,67 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  // NOVOS TESTES IMPLEMENTADOS:
+  
+  it('Adiciona múltiplas tarefas e verifica contagem', () => {
+    cy.visit('');
+    
+    const tasks = ['Tarefa 1', 'Tarefa 2', 'Tarefa 3'];
+    tasks.forEach(task => {
+      cy.get('[data-cy=todo-input]')
+        .type(`${task}{enter}`);
+    });
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', tasks.length);
+  });
+
+  it('Marca tarefa como completa e verifica estado', () => {
+    cy.visit('');
+    
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa para completar{enter}');
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('not.have.class', 'completed');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('have.class', 'completed');
+  });
+
+  it('Limpa tarefas completas', () => {
+    cy.visit('');
+    
+    // Adiciona e completa duas tarefas
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .each(($el) => {
+        cy.wrap($el).click();
+      });
+
+    // Verifica que temos 2 tarefas completas
+    cy.get('[data-cy=filter-completed-link]').click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2);
+
+    // Limpa as completas
+    cy.get('.clear-completed').click();
+
+    // Verifica que não há mais tarefas completas
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+  });
 });
